67693246877e448e5ef5763eae6ed6ba50d079ac
bulakexipei <zhuqin0331@foxmail.com>
2024-12-24 10:20:45 +0800

    Update PDFrm.py

--- PDFrm.py
+++ PDFrm.py
@@ -16,55 +16,55 @@ file_categories = {
     'test'  :   'Prep',
 }
 
-source_folder = '\\\\10.96.1.231\\Agilent LCMS-02 Report\\ZJ-LCMS-02'
-destination_folder = '\\\\10.86.43.3\\Report'
+source_folder = 'source_folder'
+destination_folder = 'destination_folder'
 data_type = 'LCMS_ZJ'
 # 创建一个文件夹，用于存放未知文件
 unknown_folder = os.path.join(source_folder, 'Unknown')
 #os.makedirs(unknown_folder, exist_ok=True)
 
-##这段代码将整个文件夹中的文件全部改名
-# def rename_pdf_files(path):
-#     # 获取目录中的PDF文件列表
-#     pdf_files = [f for f in os.listdir(path) if f.endswith(".pdf")]
+#这段代码将整个文件夹中的文件全部改名
+def rename_pdf_files(path):
+    # 获取目录中的PDF文件列表
+    pdf_files = [f for f in os.listdir(path) if f.endswith(".pdf")]
 
-#     for pdf_file in pdf_files:
-#         # 构建完整的PDF文件路径
-#         pdf_file_path = os.path.join(path, pdf_file)
+    for pdf_file in pdf_files:
+        # 构建完整的PDF文件路径
+        pdf_file_path = os.path.join(path, pdf_file)
 
-#         pdf_name, extension = os.path.splitext(pdf_file)
+        pdf_name, extension = os.path.splitext(pdf_file)
 
-#         try:
-#             # 打开PDF文件
-#             with pdfplumber.open(pdf_file_path) as pdf:
-#                 # 获取第一页的内容
-#                 page = pdf.pages[0]
-#                 text = page.extract_text()
-#                 #print(text)
-#                 # 使用正则表达式提取样本名称
-#                 sample_name_match = re.search(r'Project Name: +(.*?)\n', text)
-#                 if sample_name_match:
-#                     source_name = sample_name_match.group(1)
+        try:
+            # 打开PDF文件
+            with pdfplumber.open(pdf_file_path) as pdf:
+                # 获取第一页的内容
+                page = pdf.pages[0]
+                text = page.extract_text()
+                #print(text)
+                # 使用正则表达式提取样本名称
+                sample_name_match = re.search(r'Project Name: +(.*?)\n', text)
+                if sample_name_match:
+                    source_name = sample_name_match.group(1)
 
-#                     # 去除非法字符并替换空格为下划线
-#                     legal_name = re.sub(r'[/*?:<>|\\]', '', source_name).replace('  ', '_')
+                    # 去除非法字符并替换空格为下划线
+                    legal_name = re.sub(r'[/*?:<>|\\]', '', source_name).replace('  ', '_')
 
-#                     # 构建新文件名
-#                     new_filename = legal_name + '_' + pdf_file
-#                     pdf.close()
-#                     #print (pdf_name)
-#                     #print (new_filename)
-#                     # 重命名PDF文件
-#                     os.rename(pdf_file_path, os.path.join(path, new_filename))
-#                 else:
-#                     pdf.close()
-#                     os.rename(pdf_file_path, os.path.join(path, pdf_name + '_NoProjectName.pdf'))
-#         except Exception as e:
-#             print(f"Error renaming {pdf_file}: {str(e)}")
-#             continue  # 继续处理下一个文件
+                    # 构建新文件名
+                    new_filename = legal_name + '_' + pdf_file
+                    pdf.close()
+                    #print (pdf_name)
+                    #print (new_filename)
+                    # 重命名PDF文件
+                    os.rename(pdf_file_path, os.path.join(path, new_filename))
+                else:
+                    pdf.close()
+                    os.rename(pdf_file_path, os.path.join(path, pdf_name + '_NoProjectName.pdf'))
+        except Exception as e:
+            print(f"Error renaming {pdf_file}: {str(e)}")
+            continue  # 继续处理下一个文件
 
-# 调用函数并传入文件夹路径
-#rename_pdf_files(source_folder)
+调用函数并传入文件夹路径
+rename_pdf_files(source_folder)
 
 ##这段代码只修改没有在字典中找到值的文件的文件名
 def rename_pdf_file(pdf_file_path):
